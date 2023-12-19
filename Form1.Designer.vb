90aba92773e34fa437cd23def9f1c07551356d87
Gabriel Lao <gabe1@vt.edu>
2023-12-19 16:36:39 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        txtProductName = New TextBox()
+        Label2 = New Label()
+        txtQuantity = New TextBox()
+        submitbutton = New Button()
+        lstInventory = New ListBox()
+        txtSearchProductName = New TextBox()
+        Label3 = New Label()
+        btnSearch = New Button()
+        txtQuantityUpdate = New TextBox()
+        addStock = New Button()
+        sellStock = New Button()
+        Label6 = New Label()
+        lowstock = New Button()
+        Label4 = New Label()
+        ViewInventory = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(35, 38)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(87, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Product Name:"
+        ' 
+        ' txtProductName
+        ' 
+        txtProductName.Location = New Point(128, 29)
+        txtProductName.Name = "txtProductName"
+        txtProductName.Size = New Size(100, 23)
+        txtProductName.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(66, 72)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(56, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "Quantity:"
+        ' 
+        ' txtQuantity
+        ' 
+        txtQuantity.Location = New Point(128, 64)
+        txtQuantity.Name = "txtQuantity"
+        txtQuantity.Size = New Size(100, 23)
+        txtQuantity.TabIndex = 3
+        ' 
+        ' submitbutton
+        ' 
+        submitbutton.Location = New Point(35, 152)
+        submitbutton.Name = "submitbutton"
+        submitbutton.Size = New Size(90, 25)
+        submitbutton.TabIndex = 4
+        submitbutton.Text = "Add Product"
+        submitbutton.UseVisualStyleBackColor = True
+        ' 
+        ' lstInventory
+        ' 
+        lstInventory.FormattingEnabled = True
+        lstInventory.ItemHeight = 15
+        lstInventory.Location = New Point(35, 220)
+        lstInventory.Name = "lstInventory"
+        lstInventory.Size = New Size(120, 94)
+        lstInventory.TabIndex = 5
+        ' 
+        ' txtSearchProductName
+        ' 
+        txtSearchProductName.Location = New Point(536, 38)
+        txtSearchProductName.Name = "txtSearchProductName"
+        txtSearchProductName.Size = New Size(100, 23)
+        txtSearchProductName.TabIndex = 6
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(413, 46)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(117, 15)
+        Label3.TabIndex = 7
+        Label3.Text = "Search for a Product:"
+        ' 
+        ' btnSearch
+        ' 
+        btnSearch.Location = New Point(657, 42)
+        btnSearch.Name = "btnSearch"
+        btnSearch.Size = New Size(75, 23)
+        btnSearch.TabIndex = 8
+        btnSearch.Text = "Search"
+        btnSearch.UseVisualStyleBackColor = True
+        ' 
+        ' txtQuantityUpdate
+        ' 
+        txtQuantityUpdate.Location = New Point(128, 105)
+        txtQuantityUpdate.Name = "txtQuantityUpdate"
+        txtQuantityUpdate.Size = New Size(100, 23)
+        txtQuantityUpdate.TabIndex = 10
+        ' 
+        ' addStock
+        ' 
+        addStock.Location = New Point(144, 153)
+        addStock.Name = "addStock"
+        addStock.Size = New Size(90, 23)
+        addStock.TabIndex = 11
+        addStock.Text = "Update: Add"
+        addStock.UseVisualStyleBackColor = True
+        ' 
+        ' sellStock
+        ' 
+        sellStock.Location = New Point(253, 153)
+        sellStock.Name = "sellStock"
+        sellStock.Size = New Size(90, 23)
+        sellStock.TabIndex = 12
+        sellStock.Text = "Update: Sell"
+        sellStock.UseVisualStyleBackColor = True
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(21, 108)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(101, 15)
+        Label6.TabIndex = 15
+        Label6.Text = "Quantity Updater:"
+        ' 
+        ' lowstock
+        ' 
+        lowstock.Location = New Point(190, 291)
+        lowstock.Name = "lowstock"
+        lowstock.Size = New Size(75, 23)
+        lowstock.TabIndex = 16
+        lowstock.Text = "Search"
+        lowstock.UseVisualStyleBackColor = True
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(190, 261)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(124, 15)
+        Label4.TabIndex = 17
+        Label4.Text = "Low Stock Viewer (<5)"
+        ' 
+        ' ViewInventory
+        ' 
+        ViewInventory.Location = New Point(413, 291)
+        ViewInventory.Name = "ViewInventory"
+        ViewInventory.Size = New Size(75, 23)
+        ViewInventory.TabIndex = 18
+        ViewInventory.Text = "View"
+        ViewInventory.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(ViewInventory)
+        Controls.Add(Label4)
+        Controls.Add(lowstock)
+        Controls.Add(Label6)
+        Controls.Add(sellStock)
+        Controls.Add(addStock)
+        Controls.Add(txtQuantityUpdate)
+        Controls.Add(btnSearch)
+        Controls.Add(Label3)
+        Controls.Add(txtSearchProductName)
+        Controls.Add(lstInventory)
+        Controls.Add(submitbutton)
+        Controls.Add(txtQuantity)
+        Controls.Add(Label2)
+        Controls.Add(txtProductName)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtProductName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtQuantity As TextBox
+    Friend WithEvents submitbutton As Button
+    Friend WithEvents lstInventory As ListBox
+    Friend WithEvents txtSearchProductName As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents txtQuantityUpdate As TextBox
+    Friend WithEvents addStock As Button
+    Friend WithEvents sellStock As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lowstock As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ViewInventory As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,151 @@
+﻿Public Class Form1
+    ' Defines the class product with two properties known as name and quantity (string and integer) 
+    Class Product
+        Public Property Name As String
+        Public Property Quantity As Integer
+    End Class
+    ' Declares the inventory as a certain product
+    Private inventory As New List(Of Product)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub submitbutton_Click(sender As Object, e As EventArgs) Handles submitbutton.Click
+        ' defines the value of productname and quantity
+        Dim productName As String = txtProductName.Text
+        Dim quantity As Integer
+
+        If Integer.TryParse(txtQuantity.Text, quantity) Then
+            If quantity >= 0 Then
+                ' creates a new product 
+                Dim newProduct As New Product With {.Name = productName, .Quantity = quantity}
+
+                ' inserts the specified products into the text box
+                inventory.Add(newProduct)
+
+                ' clears the text boxes if needed
+                txtProductName.Clear()
+                txtQuantity.Clear()
+
+                ' Display the updated inventory in the ListBox
+                UpdateInventoryListBox()
+                ' shows an error if the value given is in the negatives
+            Else
+                MessageBox.Show("Please enter a non-negative quantity.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Else
+            ' shows an error if the value is not valid (ex. letters instead of numbers)
+            MessageBox.Show("Please enter a valid quantity.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+    Private Sub UpdateInventoryListBox()
+        ' clears the inventory when the button is clicked
+        lstInventory.Items.Clear()
+        For Each product In inventory
+            ' adds the quantity to the specified product
+            lstInventory.Items.Add($"{product.Name} - {product.Quantity}")
+        Next
+    End Sub
+    ' label for instructions
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtProductName.TextChanged
+
+    End Sub
+    ' label for instructions
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+
+
+    End Sub
+    'label for instructions
+    Private Sub txtSearchProductName_TextChanged(sender As Object, e As EventArgs) Handles txtSearchProductName.TextChanged
+
+    End Sub
+    ' makes a new instance for the button used to search for specific products
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        ' defines the buttons and the text boxes needed to find each product and quantity
+        Dim searchProductName As String = txtSearchProductName.Text
+        Dim foundProduct As Product = inventory.Find(Function(p) p.Name = searchProductName)
+        ' if the product is found, then show on a messagebox
+        If foundProduct IsNot Nothing Then
+            MessageBox.Show($"Product Name: {foundProduct.Name}{vbCrLf}Quantity: {foundProduct.Quantity}", "Found Product", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ' if not, then show an error saying that the product was not found
+        Else
+            MessageBox.Show("Product was not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+    Private Sub addStock_Click(sender As Object, e As EventArgs) Handles addStock.Click
+        ' defines the quantity to add integer, while using the same productname textbox
+        Dim productName As String = txtProductName.Text
+        Dim quantityToAdd As Integer
+        ' if it is greater than 0, then it will attempt to find the product needed ij the inventory list
+        If Integer.TryParse(txtQuantityUpdate.Text, quantityToAdd) Then
+            If quantityToAdd > 0 Then
+                ' finds the product in the inventory list
+                Dim productToUpdate As Product = inventory.Find(Function(p) p.Name = productName)
+                ' used to update the stock or quantity
+                If productToUpdate IsNot Nothing Then
+                    productToUpdate.Quantity += quantityToAdd
+                    MessageBox.Show($"Stock added. New Quantity: {productToUpdate.Quantity}", "The Stock is now Updated", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Else
+                    ' if not found, show error saying it is not found
+                    MessageBox.Show("Product was not found.", "Product was Not Found", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                End If
+
+                ' clears the text boxes as needed in the list
+                txtProductName.Clear()
+                txtQuantityUpdate.Clear()
+
+                ' updates the inventory display/list
+                UpdateInventoryListBox()
+            Else
+                ' error if you do not add a quantity that is positive
+                MessageBox.Show("Please enter a positive quantity to add.", "Invalid Value", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Else
+            ' shows another error if the quantity is not a valid number
+            MessageBox.Show("Please enter a valid quantity to add.", "Invalid Value", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub lowstock_Click(sender As Object, e As EventArgs) Handles lowstock.Click
+        ' creates a list for the low stock products
+        Dim lowStockProducts As New List(Of Product)
+
+        ' shows the inventory list 
+        For Each product In inventory
+            If product.Quantity < 5 Then
+                lowStockProducts.Add(product)
+            End If
+        Next
+
+        ' displays/shows the list of the low stocked products
+        If lowStockProducts.Count > 0 Then
+            Dim lowStockMessage As String = "Low Stock Products:" & vbCrLf
+            For Each lowStockProduct In lowStockProducts
+                lowStockMessage &= $"{lowStockProduct.Name} - {lowStockProduct.Quantity}" & vbCrLf
+            Next
+            ' creates a messagebox saying which products are considered to be low stocked
+            MessageBox.Show(lowStockMessage, "Low Stock Products", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            ' shows if none of the products are less than 5 (aka low stocked)
+            MessageBox.Show("Currently no low stock products found.", "Low Stock Products", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+    Private Sub ViewInventory_Click(sender As Object, e As EventArgs) Handles ViewInventory.Click
+        ' Create a message to display the complete inventory
+        Dim inventoryMessage As String = "Complete Inventory:" & vbCrLf
+        For Each product In inventory
+            inventoryMessage &= $"{product.Name} - {product.Quantity}" & vbCrLf
+        Next
+
+        ' Display the complete inventory
+        If inventory.Count > 0 Then
+            MessageBox.Show(inventoryMessage, "View All Inventory", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Inventory is currently empty.", "Currently in the Inventory:", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+End Class
+
